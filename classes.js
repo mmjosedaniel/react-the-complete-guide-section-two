711408c6b739ea1235de6186644551c9397c58c2
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-01-11 22:32:43 -0500

    16 Understanding classes

---
+++ classes.js
@@ -0,0 +1,28 @@
+// 16 understanding classes
+
+class Human {
+    constructor() {
+        this.gender = 'Male';
+    }
+
+    printGender() {
+        console.log(this.gender);
+    }
+}
+
+class Person extends Human {
+    constructor() {
+        super();
+        this.name = 'Max';
+        this.gender = 'Female'
+    }
+
+    printMyName() {
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+
+person.printMyName();
+person.printGender();
